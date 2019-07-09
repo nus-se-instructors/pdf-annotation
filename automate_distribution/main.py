1b10b98aa9c720fb23ee41b6c23176a6e28dbb43
Lee Yi Jie Joel <joel@Lees-MacBook-Pro-2.local>
2019-07-09 11:59:01 -0700

    Add sample input file and update encryption for task1

--- automate_distribution/main.py
+++ automate_distribution/main.py
@@ -1,24 +1,37 @@
 import pandas as pd
 import csv
 import hashlib
+import pyminizip
+import os
 
 from Crypto.Cipher import DES
 from collections import defaultdict
-from os import listdir
 
 INPUT_FILE = "./inputs/input.csv"
+NAMES = "Names"
+TEAMS = "Teams"
+PASSWORD = "Password"
+ENCODING = "utf-8"
 
 
-def encrypt_files_in_directory():
+def encrypt_files_in_directory(df):
+    # Loop through the output csv file
+    # Names, password, hash
+    # for each person, encode the jar file in the second column with the jar
+    for index, jarfile in enumerate(df[TEAMS]):
+        src_filepath = os.path.join("./inputs/input_files/", jarfile + ".jar")
+        dst_filepath = os.path.join("./outputs/output_files/", jarfile + ".jar")
+        password = df[PASSWORD][index]
+        import pdb
 
-    pass
-
-    # Encrypt each file with a password
+        pdb.set_trace()
+        pyminizip.compress(src_filepath, None, dst_filepath, password, 0)
 
 
 if __name__ == "__main__":
     df = pd.read_csv(INPUT_FILE)
-    df["Password"] = df["Names"].apply(
-        lambda x: hashlib.md5(x.encode("utf-8")).hexdigest()
+    df[PASSWORD] = df[NAMES].apply(
+        lambda x: hashlib.md5(x.encode(ENCODING)).hexdigest()
     )
     df.to_csv("./outputs/output.csv")
+    encrypt_files_in_directory(df)
