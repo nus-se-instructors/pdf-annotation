c4baef444a98f0b88170965bf515a4a9287a366f
Lee Yi Jie Joel <joel@Lees-MacBook-Pro-2.local>
2019-07-09 11:35:59 -0700

    Refactor main file for task1

--- automate_distribution/main.py
+++ automate_distribution/main.py
@@ -1,53 +1,24 @@
 import pandas as pd
 import csv
 import hashlib
+
 from Crypto.Cipher import DES
 from collections import defaultdict
-
+from os import listdir
 
 INPUT_FILE = "./inputs/input.csv"
 
 
-def encrypt_file():
+def encrypt_files_in_directory():
+
     pass
 
-
-def generate_password_list(student_names):
-    """
-    Takes in a series of student names and returns a list of dictionaries
-    """
-    password_list = []
-    for name in student_names:
-        password_list.append(
-            {"Name": name, "Password": hashlib.md5(name.encode("utf-8")).hexdigest()}
-        )
-    return password_list
-
-
-def write_dict_to_csv(password_list):
-    """
-    Write dictionary to output csv file
-    """
-    csv_columns = ["Name", "Password"]
-    csv_file = "./outputs/output.csv"
-    try:
-        with open(csv_file, "w") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-            writer.writeheader()
-            for item in password_list:
-                writer.writerow(item)
-    except IOError:
-        print("I/O error")
-        return False
-    return True
+    # Encrypt each file with a password
 
 
 if __name__ == "__main__":
     df = pd.read_csv(INPUT_FILE)
-    names = df["Names"]
-    password_list = generate_password_list(names)
-    if write_dict_to_csv(password_list):
-        print("Success")
-    else:
-        print("Please Check your csv file")
-    # print(teams)
+    df["Password"] = df["Names"].apply(
+        lambda x: hashlib.md5(x.encode("utf-8")).hexdigest()
+    )
+    df.to_csv("./outputs/output.csv")
