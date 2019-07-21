9508101233c1366bac18e74f655dce172485b51e
Lee Yi Jie Joel <joel@Lees-MacBook-Pro-2.local>
2019-07-21 16:05:17 -0700

    Add add_toc function

--- task_two/main.py
+++ task_two/main.py
@@ -1,8 +1,4 @@
 import logging
-from matplotlib.backends.backend_pdf import PdfPages
-import PyPDF2
-from tika import parser
-
 
 # Add Page numbers
 # https://stackoverflow.com/questions/25164871/how-to-add-page-numbers-to-pdf-file-using-python-and-matplotlib
@@ -18,57 +14,58 @@ from tika import parser
 # How to muck around with PDFs:
 # https://automatetheboringstuff.com/chapter13/
 
-
-from reportlab.pdfgen import canvas
-from PyPDF2 import PdfFileReader, PdfFileMerger
-from pdfrw import PdfReader, PdfWriter, PageMerge
+import fitz
 
 
 TEXTBOOK = "./inputs/textbook.pdf"
 OVERLAY = "./outputs/overlay.pdf"
 OUTPUT = "./outputs/textbook_output.pdf"
+DEFAULT_ERROR_MESSAGE = "%s phase failed"
 
 
-def create_overlay(num_pages):
-    """
-    Create a multi-page document
-    """
-    c = canvas.Canvas(OVERLAY)
-
-    for i in range(num_pages):
-        page_num = c.getPageNumber()
-        text = "%s" % page_num
-        c.drawString(800, 40, text)
-        c.showPage()
-    c.save()
+def add_page_numbers(doc):
+    return True
 
 
-def watermarker(textbook, overlay, output):
-    base_pdf = PdfReader(textbook)
-    watermark_pdf = PdfReader(overlay)
+def add_toc(doc):
+    SECTION_DELIMITER = "SECTION: "
+    LEVEL = 2
+    # Initial level one heading
+    toc = [[1, "Textbook", 0]]
+    for page_number in range(doc.pageCount):
+        page_text = doc.getPageText(page_number)
+        section_index = page_text.find(SECTION_DELIMITER)
+        if section_index != -1:
+            section_title = page_text[section_index:].split("\n")[0]
+            logging.info("Section title is %s" % section_title)
+            # Adjust for the fact that pages are 0 indexed
+            toc.append([LEVEL, section_title, page_number + 1])
+    logging.info("The items in the table of contents are:" % toc)
+    doc.setToC(toc)
+    doc.save(OUTPUT)
+    return True
 
-    for page in range(len(base_pdf.pages)):
-        mark = watermark_pdf.pages[page]
-        merger = PageMerge(base_pdf.pages[page])
-        merger.add(mark).render()
 
-    writer = PdfWriter()
-    writer.write(output, base_pdf)
-
-
-def extract_text(TEXTBOOK):
-    text = textract.process(TEXTBOOK)
-    print(text)
-
-
-# ----------------------------------------------------------------------
+def add_index(doc):
+    raise NotImplementedError("Index addition not implemented")
 
 
 if __name__ == "__main__":
-    pdf = PdfFileReader(open(TEXTBOOK, "rb"))
-    num_pages = pdf.getNumPages()
-    create_overlay(num_pages)
-    watermarker(TEXTBOOK, OVERLAY, OUTPUT)
-    raw = parser.from_file(TEXTBOOK)
-    with open("pdftotext.txt", "w") as output:
-        output.write(raw["content"])
+    doc = fitz.open(TEXTBOOK)
+    try:
+        add_page_numbers(doc)
+    except Error as e:
+        logging.info(e)
+        raise Exception("Page number addition failed")
+
+    try:
+        add_toc(doc)
+    except Error as e:
+        logging.info(e)
+        raise Exception("Bookmark addition failed")
+
+    try:
+        add_index(doc)
+    except Error as e:
+        logging.info(e)
+        raise Exception("Index addition failed")
